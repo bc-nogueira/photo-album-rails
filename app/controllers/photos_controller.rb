510a87b39d91941b4897dc34c65906c6a9e41623
bc-nogueira <bcnogueira@id.uff.br>
2017-09-19 00:35:49 -0300

    Adding show photo page.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,11 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :destroy]
+
   def index
     @photos = Photo.all
   end
+
+  def show; end
 
   def new
     @photo = Photo.new
@@ -19,12 +23,15 @@ class PhotosController < ApplicationController
   end
 
   def destroy
-    photo = Photo.find(params[:id])
-    photo.destroy
+    @photo.destroy
     redirect_to photos_path
   end
 
   private
+
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
 
   def photo_attributes
     params.require(:photo).permit(:title, :date, :location, :photo)
